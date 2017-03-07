b7e67aaa1370ac3329701a6f29366fe651bd0546
webbdm <gwebb762@gmail.com>
2017-03-06 19:57:40 -0600

    added functions

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("test");
+
+function executeThisCodeAfterFileLoaded(){
+	console.log("worked");
+
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("failed");
+
+}
+
+
+var dinoRequest = new XMLHttpRequest();
+dinoRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+dinoRequest.addEventListener("error", executeThisCodeAfterFileFails);
+dinoRequest.open("GET","dinosaurs.json");
+dinoRequest.send();
+console.log("dinoRequest", dinoRequest);
